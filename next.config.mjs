7ba86482d5123166988aef53ea4d5edd69eddf8c
Shubham Kandpal <subhamkandpal799@gmail.com>
2025-06-22 16:21:52 +0530

    ok done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ['placehold.co'],
+//   },
+// };
+
+// export default nextConfig;
+
+
+
+// next.config.mjs
+
 const nextConfig = {
   images: {
     domains: ['placehold.co'],
